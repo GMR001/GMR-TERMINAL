251eac2638dc6dd7a738c9824d09e8f3a37a8385
GMR001 <uzbktinku@gmail.com>
2026-01-29 17:21:42 +0500

    Add files via upload
    THIS IS FOR VENDOR INFORMATION COLLECTION.

---
+++ script.js
@@ -0,0 +1,13 @@
+const text ="GMR TERMINAL ACTIVATE ...";
+let index = 0;
+const speed = 80;
+
+function typeEffect() {
+    if (index < text.length) {
+        document.getElementById("typeText").textContent += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, speed);
+    }
+}
+
+typeEffect();
